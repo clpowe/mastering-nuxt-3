18834ce7d756924a983c0436ac22ac78a15e4987
clpowe <33496540+clpowe@users.noreply.github.com>
2022-11-08 20:38:12 -0500

    shamfull hoist

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+	const props = defineProps({
+		videoId: {
+			type: Number,
+			required: true
+		}
+	})
+</script>
+
+<template>
+	<iframe
+		width="560"
+		height="315"
+		:src="`https://player.vimeo.com/video/${props.videoId}`"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+		title="Video content"
+	></iframe>
+</template>
+
+<style scoped>
+	iframe {
+		border: 0px;
+	}
+</style>
