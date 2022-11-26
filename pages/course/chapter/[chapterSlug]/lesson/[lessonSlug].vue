c8a981ae1e194515fbbc0ec799a3cc676d271cc7
clpowe <33496540+clpowe@users.noreply.github.com>
2022-11-25 21:34:57 -0500

    feat - supabase

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -35,33 +35,38 @@
 	const route = useRoute()
 
 	definePageMeta({
-		validate({ params }) {
-			const course = useCourse()
+		middleware: [
+			function ({ params }, from) {
+				const course = useCourse()
 
-			const chapter = course.chapters.find(
-				(chapter) => chapter.slug === params.chapterSlug
-			)
+				const chapter = course.chapters.find(
+					(chapter) => chapter.slug === params.chapterSlug
+				)
 
-			if (!chapter) {
-				return createError({
-					statusCode: 404,
-					message: 'Chapter not found'
-				})
-			}
+				if (!chapter) {
+					return abortNavigation(
+						createError({
+							statusCode: 404,
+							message: 'Chapter not found'
+						})
+					)
+				}
 
-			const lesson = chapter.lessons.find(
-				(lesson) => lesson.slug === params.lessonSlug
-			)
+				const lesson = chapter.lessons.find(
+					(lesson) => lesson.slug === params.lessonSlug
+				)
 
-			if (!lesson) {
-				return createError({
-					statusCode: 404,
-					message: 'Lesson not found'
-				})
-			}
-
-			return true
-		}
+				if (!lesson) {
+					return abortNavigation(
+						createError({
+							statusCode: 404,
+							message: 'Lesson not found'
+						})
+					)
+				}
+			},
+			'auth'
+		]
 	})
 
 	const chapter = computed(() => {
