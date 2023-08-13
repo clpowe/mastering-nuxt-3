e0a9d4fc9c18801d6f0b8f29e62310a89ceb3480
clpowe <clpowe@gmail.com>
2023-08-12 22:40:20 -0400

    lesson 1-14

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+	defineProps({
+		videoId: {
+			type: Number,
+			required: true
+		}
+	})
+</script>
+
+<template>
+	<iframe
+		width="560"
+		height="315"
+		:src="`https://player.vimeo.com/video/${videoId}`"
+		title="Video player"
+		style="border: 0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+	></iframe>
+</template>
