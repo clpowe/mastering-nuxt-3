87012012794d4741fa180ae4f44ef5c88cd7f75e
clpowe <clpowe@gmail.com>
2023-08-12 23:09:29 -0400

    router options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt']
+	modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+	router: {
+		options: {
+			strict: true
+		}
+	}
 })
